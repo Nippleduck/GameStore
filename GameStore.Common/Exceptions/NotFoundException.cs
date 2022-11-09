742c58e4ec3170918126d4756c6ba9a70164c849
Dmytro Khmelyk <delinger360@gmail.com>
2022-11-09 21:54:17 +0200

    created custom exception for "entity not found" scenario

---
+++ GameStore.Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Common.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base() { }
+
+        public NotFoundException(string name) : base($"Entity {name} was not found") { }
+    }
+}

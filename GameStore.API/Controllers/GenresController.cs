84c4648873c5e604c121ebf8472cf9776c1fb58f
Dmytro Khmelyk <delinger360@gmail.com>
2022-11-15 15:39:25 +0200

    added genres controller with base endpoints

---
+++ GameStore.API/Controllers/GenresController.cs
@@ -0,0 +1,40 @@
+﻿using GameStore.Application.Interfaces;
+using GameStore.Application.Models.Genres.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        public GenresController(IGenreService genreService)
+        {
+            this.genreService = genreService;
+        }
+
+        private readonly IGenreService genreService;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await genreService.GetByIdAsync(id));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await genreService.GetAllAsync();
+
+            return result.Any() ? Ok(result) : NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddGenreRequest request)
+        {
+            var result = await genreService.AddAsync(request);
+
+            return CreatedAtAction(nameof(request), result);
+        }
+    }
+}
